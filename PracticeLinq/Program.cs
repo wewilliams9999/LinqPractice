71e325cda6542d3eadb959692e3dcccac6efa1e1
wewilliams9999 <williamswallise@gmail.com>
2022-04-09 20:56:06 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>();
+            videoGames.Add("Tetris");
+            videoGames.Add("Mario Cart");
+            videoGames.Add("Sonic the Hedgehog");
+            var videoGamesOrderedByLength = videoGames.OrderBy(game => game.Length);
+
+            foreach (var game in videoGamesOrderedByLength)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
